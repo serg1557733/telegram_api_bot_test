fe4f4b6a029d950f43fdaace0732966d1aaa6a26
serg1557733 <serg1557733@ukr.net>
2022-08-26 22:54:59 +0200

    add regexp and keywords

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const  { Telegraf } = require('telegraf');
 const express = require('express')
 
 const token = '5717084656:AAET0Vzk61yjkhEO18yltR1GhvkDYtheZdg'
+
 const chatId = '-684878853'
 
 const bot = new Telegraf(token, {polling: true})
@@ -9,18 +10,46 @@ const bot = new Telegraf(token, {polling: true})
 const app = express()
 const port = 3030
 
+const  RegExpLink = /(([a-z]+:\/\/)?(([a-z0-9\-]+\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel|local|internal))(:[0-9]{1,5})?(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&amp;]*)?)?(#[a-zA-Z0-9!$&'()*+.=-_~:@/?]*)?)(\s+|$)/gi;
+
 
 bot.on('message', async (ctx) => {
-  console.log(ctx.message.message_id)
+
   if(ctx.message.text) {
     const str = ctx.message.text.toLowerCase();
-    if(str.includes('iptv') || str.includes('playlist')|| str.includes('77')){
-      ctx.deleteMessage(ctx.message.message_id)
+    if(str.includes('iptv')){
+        ctx.reply(`Message from ${ctx.update.message.from.first_name} edited by BOT
+         ${ctx.message.text.replaceAll('iptv', '****')}`)
+        ctx.deleteMessage(ctx.message.message_id)
+        return
 
-      //ctx.telegram.editMessageCaption(chatId, 350,350,350); 
     }
+
+    if(str.includes('iptv')){
+        ctx.reply(`Message from ${ctx.update.message.from.first_name} edited by BOT
+         ${ctx.message.text.replaceAll('шаринг', '****')}`)
+        ctx.deleteMessage(ctx.message.message_id)
+        return
+
+    }
+
+    if(str.includes('playlist')){
+        ctx.reply(`Message from ${ctx.update.message.from.first_name} edited by BOT
+         ${ctx.message.text.replaceAll('playlist', '****')}`)
+        ctx.deleteMessage(ctx.message.message_id)
+
+    }
+
+
+    if(RegExpLink.test(str)){
+        console.log(RegExpLink.test(str))
+        ctx.reply(`Message from ${ctx.update.message.from.first_name} edited by BOT
+        ${ctx.message.text.replace(RegExpLink, 'http****.***')}`)
+        ctx.deleteMessage(ctx.message.message_id)
+        return
+
+     }
   }
-  
 
    if(ctx.message.document){
       const isPlaylist = ctx.message.document.file_name.includes('.m3u')
@@ -32,7 +61,6 @@ bot.on('message', async (ctx) => {
   })
 
 bot.launch()
-
 
 
 app.get('/', (req, res) => {
